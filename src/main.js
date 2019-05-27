6aa742f9375ebe12790d9629167a3faa943746f9
Edgar Valdez <edgarvaldez99@gmail.com>
2019-05-26 21:43:18 -0400

    chore: agregada la configuración para vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import './plugins/vuetify'
+import { sync } from 'vuex-router-sync'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+sync(store, router)
 
 new Vue({
   router,
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#88691A',
+    secondary: '#083759'
+  }
+})
