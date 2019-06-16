37d1b5b8b56fecc5941ddfdfe9c74e3eeaca7c16
Edgar Valdez <edgarvaldez99@gmail.com>
2019-06-16 10:30:14 -0400

    feat: funcionalidad para cerrar sesión actualizada

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,39 +3,57 @@ import router from '@/router'
 
 const initialState = localStorage.session ? JSON.parse(localStorage.getItem('session')) : {
   user: null,
+  userSelected: null,
   isAuthenticated: false
 }
 
 const updateSessionInLocalStorage = session => localStorage.setItem('session', JSON.stringify(session))
 
+const signOut = _ => {
+  router.push('/')
+  window.location.reload()
+}
+
 export default {
   state: { ...initialState },
   mutations: {
     setUser: (state, payload) => (state.user = payload),
+    setUserSelected: (state, payload) => (state.userSelected = payload),
     setIsAuthenticated: (state, payload) => (state.isAuthenticated = payload)
   },
   actions: {
     updateAllState: ({ commit, dispatch }, user) => {
       commit('setUser', user)
+      commit('setUserSelected', user)
       commit('setIsAuthenticated', !!user)
       dispatch('updateSessionInLocalStorage')
     },
+    createUser: ({ dispatch }, { email, password }) =>
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(authData => {
+          dispatch('updateAllState', authData.user)
+          return authData
+        }),
     userLogin: ({ dispatch }, { email, password }) =>
       auth
         .signInWithEmailAndPassword(email, password)
-        .then(user => dispatch('updateAllState', user)),
+        .then(authData => dispatch('updateAllState', authData.user)),
     userLogout: _ =>
       auth
         .signOut()
-        .then(_ => router.push('/'))
-        .catch(_ => router.push('/')),
+        .then(_ => signOut())
+        .catch(_ => signOut()),
     updateSessionInLocalStorage: ({ state }) => updateSessionInLocalStorage(state),
     loginWithGoogle: ({ dispatch }) => {
       const provider = new firebase.auth.GoogleAuthProvider()
       auth.signInWithPopup(provider).then(result => dispatch('updateAllState', result.user))
-    }
+    },
+    setUserSelected: ({ commit }, user) => commit('setUserSelected', user)
   },
   getters: {
-    isAuthenticated: state => state.isAuthenticated
+    userIsAdmin: state => state.user.getIdTokenResult && state.user.getIdTokenResult(),
+    isAuthenticated: state => state.isAuthenticated,
+    userSelected: state => state.userSelected
   }
 }
