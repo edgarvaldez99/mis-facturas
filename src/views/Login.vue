d7dc2c8ee503b2868d82940c7897ad22ab8b7bf5
Edgar Valdez <edgarvaldez99@gmail.com>
2019-06-16 02:50:51 -0400

    feat: agregada funcionalidad para creación de usuario

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,8 +8,12 @@ v-layout(align-center justify-center)
         v-form(ref='form', v-model='valid', lazy-validation)
           v-text-field(v-model='form.email', :rules='validators.email', label='Correo electrónico', required, :disabled="sending")
           v-text-field(v-model='form.password', type="password", :counter='6', :rules='validators.password', label='Contraseña', required, :disabled="sending")
-          v-btn(:disabled='!valid', color='success', @click='validate') Iniciar sesión
+          v-text-field(v-if='signUp', v-model='form.confirmPassword', type="password", :counter='6', :rules='validators.confirmPassword', :error-messages='errorMessages.confirmPassword', label='Confirmar contraseña', required, :disabled="sending")
+          v-btn(:disabled='!valid', color='success', @click='validate') {{ signUp ? 'Crear cuenta' : 'Iniciar sesión' }}
           v-btn(color='#d34836', @click='loginWithGoogle') Iniciar con Google
+          div
+            v-btn(v-if='!signUp' @click='signUp = true') Registrarse
+            v-btn(v-if='signUp' @click='signUp = false') Ingresar
 
 </template>
 
@@ -21,6 +25,7 @@ export default {
     error: '',
     valid: true,
     sending: false,
+    signUp: false,
     form: {
       email: '',
       password: ''
@@ -33,24 +38,50 @@ export default {
       password: [
         v => !!v || 'La contraseña es requerida',
         v => (v && v.length >= 6) || 'La contraseña debe tener al menos 6 caracteres'
+      ],
+      confirmPassword: [
+        v => !!v || 'La contraseña es requerida',
+        v => (v && v.length >= 6) || 'La contraseña debe tener al menos 6 caracteres'
       ]
+    },
+    errorMessages: {
+      confirmPassword: []
     }
   }),
+  watch: {
+    'form.confirmPassword' (val) {
+      const errors = []
+      this.form.password !== this.form.confirmPassword && errors.push('Ambas contraseñas no coinciden')
+      this.errorMessages.confirmPassword = errors
+    }
+  },
   methods: {
-    ...mapActions([ 'userLogin', 'loginWithGoogle' ]),
+    ...mapActions([ 'createUser', 'userLogin', 'loginWithGoogle' ]),
     validate () {
       this.error = ''
       if (this.$refs.form.validate()) {
         this.sending = true
-        this.userLogin(this.form)
-          .then(_ => {
-            this.sending = false
-            this.$router.push({ name: 'forms' })
-          })
-          .catch(err => {
-            this.sending = false
-            this.error = err.message
-          })
+        if (this.signUp) {
+          this.createUser(this.form)
+            .then(user => {
+              this.sending = false
+              this.$router.push({ name: 'forms' })
+            })
+            .catch(err => {
+              this.sending = false
+              this.error = err.message
+            })
+        } else {
+          this.userLogin(this.form)
+            .then(_ => {
+              this.sending = false
+              this.$router.push({ name: 'forms' })
+            })
+            .catch(err => {
+              this.sending = false
+              this.error = err.message
+            })
+        }
       }
     }
   }
